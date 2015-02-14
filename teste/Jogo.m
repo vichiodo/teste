bcf3edea069b69e05e298a757e5012350c64ed12
rafael hieda <41241789@mackenzista.com.br>
2015-02-14 18:40:52 -0200

    comentei o codigo do jogo rodando

--- teste/Jogo.m
+++ teste/Jogo.m
@@ -36,9 +36,11 @@
     //funcionando, mas precisei implementar dois metodos em especial, o imprimirOverride pra corrigir a conversão
     //removerTudo que remove a lista inteira para ser preenchida pelo jogador na filaPlayer
     NSInteger choice;
+    //aloca uma fila para as jogadas do jogador.
     Fila *filaPlayer = [[Fila alloc] init];
     do
-    {   if([fila elementos] == nil)
+    {   // primeiro elemento do jogo roda este if
+        if([fila elementos] == nil) 
         {
             [fila enfileirar: [NSNumber numberWithUnsignedLong:[self createRandomNumber]]];
             [NSThread sleepForTimeInterval:2.0f];
@@ -47,12 +49,23 @@
             scanf("%lu", &choice);
             [filaPlayer enfileirar: [NSNumber numberWithUnsignedLong: choice]];
         }
-        else{
+        else{ 
+            // os proximos numeros a serem jogados rodam neste else aqui
+            
+            //  a fila do jogador tem que ser desenfileirada a cada jogada para ser comparada com a fila do jogo, pois há um metodo de comparação entre elas
             [filaPlayer desenfileirarTudo];
-            [fila enfileirar: [NSNumber numberWithUnsignedLong:[self createRandomNumber]]];
+            
+            // enfileira a sequencia de jogadas geradas
+            [fila enfileirar: [NSNumber numberWithUnsignedLong:[self createRandomNumber]]]; 
+            
+             // converte cada unsigned long em um numero que corresponde a jogada e imprime a sequencia da fila do jogo
             [fila imprimirOverride];
+            
+            //dá aquele tempo para memorizar a jogada e limpa a tela
             [NSThread sleepForTimeInterval:2.0f];
             [self clearScreen];
+            
+            // pede para informar a sua escolha, conta a quantidade de elementos da fila do jogo para que possamos preencher a fila de jogadas do jogador.
             printf("\n Sua escolha: \n");
             int count = (int) [[fila elementos]count];
             for(int i = 0; i < count; i++)
@@ -61,10 +74,11 @@
                 [filaPlayer enfileirar: [NSNumber numberWithUnsignedLong: choice]];
             }
         }
+        //compara se o a sequencia de jogadas geradas pelo jogo é igual a fila de jogadas do jogador , se sim, cria-se mais uma rodada e incrementa-se de mais uma jogada.
     } while([self check:filaPlayer] == true);
     NSLog(@"Game over!!!!");
 }
-
+//uma função de checagem da fila
 -(bool) check:(Fila *) playerFila
 {
     if([[fila elementos]isEqualToArray: [playerFila elementos]])
